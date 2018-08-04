e952d6c9ff672d3f4bca238281d9179563498818
Cdvx <cdvx@grizzly.com>
2018-08-04 16:54:14 +0300

    user profile page stage, database schema edited

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,9 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-#from flask_script import Manager
+from flask_script import Manager
 from flask_login import LoginManager
 
-from app import login
 
 
 
@@ -15,7 +14,9 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+login = LoginManager(app)
 login.login_view = "login"
+manager = Manager(app)
 
 
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -33,11 +33,14 @@ class User(UserMixin, db.Model):
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
 
+		
+
 class Post(db.Model):
 	id  = db.Column(db.Integer, primary_key=True)
 	body = db.column(db.String(140))
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 	def __repr__(self):
 		return '<Post {}>'.format(self.body)
