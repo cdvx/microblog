99a91b6e2895fb792ee3a6707dcd55fa7a7bc03c
Cdvx <cdvx@grizzly.com>
2018-08-04 22:08:44 +0300

    further improvements on user profiles

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField,SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField,SubmitField,TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+
 from app.models import User
 
 
@@ -28,3 +29,7 @@ class RegistrationForm(FlaskForm):
 		if user is not None:
 			raise ValidationError('Please use a different email')
 			
+class EditProfileForm(FlaskForm):
+	username = StringField('Username',validators=[DataRequired()])
+	about_me = TextAreaField('About_me', validators=[Length(min=0, max=140)])
+	submit = SubmitField('submit')
